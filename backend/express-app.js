111f0a147cdec407d995ea00986be124299c2162
Deepak Davis <you@example.com>
2023-02-27 18:33:20 +0530

    fix: index html path for build

--- backend/express-app.js
+++ backend/express-app.js
@@ -10,4 +10,17 @@ module.exports = (app) => {
 
   customer(app);
   loan(app);
+
+  // Serve frontend
+  if (process.env.NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname, "../frontend/build")));
+
+    app.get("*", (req, res) =>
+      res.sendFile(
+        path.resolve(__dirname, "../", "frontend", "build", "index.html")
+      )
+    );
+  } else {
+    app.get("/", (req, res) => res.send("Please set to production"));
+  }
 };
